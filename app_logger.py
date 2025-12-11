6037e834e55be301142eee0925f5a7abb2c11a57
Najeeb Vayal Valappil <najeeb.vayal-valappil.ext@flender-group.com>
2025-12-11 21:01:12 +0100

    correct log formatting

--- app_logger.py
+++ app_logger.py
@@ -5,7 +5,7 @@ def get_logger(name: str = "backup") -> logging.Logger:
     if not logger.handlers:
         handler = logging.FileHandler(f"/var/log/{name}.log")
         formatter = logging.Formatter(
-            '%(levelname)-8s %(asctime)s - %(module)s - %(filename)s:%(lineno)d - %(message)s'
+            '%(asctime)s, %(levelname)-8s,%(module)s,%(filename)s:%(lineno)d,%(message)s'
         )
         handler.setFormatter(formatter)
         logger.addHandler(handler)
