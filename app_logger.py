420757e63e5609e894d8c7cdd7a148ffcfd1a847
Najeeb Vayal Valappil <najeeb.vayal-valappil.ext@flender-group.com>
2025-12-18 07:26:04 +0000

    update logging scheme

--- app_logger.py
+++ app_logger.py
@@ -3,10 +3,10 @@ import logging
 def get_logger(name: str = "backup", level: int = logging.INFO) -> logging.Logger:
     logger = logging.getLogger(name)
     if not logger.handlers:
-        handler = logging.FileHandler(f"{name}.log")
+        handler = logging.FileHandler(f"../logs/{name}.log",encoding='utf-8')
         formatter = logging.Formatter(
-            '{"TimeGenerated": "%(asctime)s", "level": "%(levelname)s", "module": "%(module)s", "file": "%(filename)s", "line": %(lineno)d, "message": "%(message)s"}',
-            datefmt='%Y-%m-%d %H:%M:%S'
+            '%(asctime)s,%(levelname)s,%(module)s,%(filename)s,%(lineno)d,%(message)s',
+            datefmt='%Y-%m-%dT%H:%M:%S%z'
         )
         handler.setFormatter(formatter)
         logger.addHandler(handler)
