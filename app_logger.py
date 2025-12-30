8a8ad37012cb741f57c2401b379894d67d448cb4
najeebvv <79649322+najeebvv@users.noreply.github.com>
2025-12-30 12:08:24 +0100

    Added App logging (#1)
    * - added logging to backup
    
    * correct log formatting
    
    * fixed logging format and filename
    
    * - json structure for logging
    - read atlassian token from keyvault secret
    
    * update requirements to include keyvault
    
    * update logging scheme
    
    * fix import
    
    * update description
    
    ---------
    
    Co-authored-by: Najeeb Vayal Valappil <najeeb.vayal-valappil.ext@flender-group.com>

---
+++ app_logger.py
@@ -0,0 +1,14 @@
+import logging
+
+def get_logger(name: str = "backup", level: int = logging.INFO) -> logging.Logger:
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        handler = logging.FileHandler(f"../logs/{name}.log",encoding='utf-8')
+        formatter = logging.Formatter(
+            '%(asctime)s,%(levelname)s,%(module)s,%(filename)s,%(lineno)d,%(message)s',
+            datefmt='%Y-%m-%dT%H:%M:%S%z'
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    logger.setLevel(level)
+    return logger
